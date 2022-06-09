58def5bc47079478b7f96d9327772f162c4ef5a1
Andrew Richards <andrew.richards67@gmail.com>
2022-06-09 16:16:59 -0400

    completed problem 876
    cool implementation, leetcode claims it completes in 0ms which seems improbable but it is pretty fast

---
+++ src/p876_MiddleOfTheLinkedList/Solution.java
@@ -0,0 +1,17 @@
+package p876_MiddleOfTheLinkedList;
+
+public class Solution {
+	public ListNode middleNode(ListNode head) {
+        ListNode curr = head;
+        ListNode midNode = head;
+        boolean changeMid = true;
+		
+        while(curr.next != null) {
+        	curr = curr.next;
+        	if(changeMid) midNode = midNode.next;
+        	changeMid = !changeMid;
+        }
+        
+		return midNode;
+    }
+}
